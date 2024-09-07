7e6ef461efebd5ce03feba0e475c4547bf07eb79
Daria <sigulia39@gmail.com>
2024-09-07 15:32:48 +0300

    добавлен restclient

--- restclient/restclient.py
+++ restclient/restclient.py
@@ -7,6 +7,7 @@ import allure
 import json
 
 
+
 def allure_attach(fn):
     def wrapper(*args, **kwargs):
         body = kwargs.get('json')
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 
+
 REQUIRES = [
     'curlify',
     'requests',
